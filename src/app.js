1a37ebf22363d05ef4dbaa043577bd212cbf394c
zmuhsenz <zmohamedmohsnz@gmail.com>
2026-09-01 16:12:56 +0300

    feat: initialize the express app

---
+++ src/app.js
@@ -0,0 +1,8 @@
+// ─── Import Modules ─────────────────────────────────────────────────────────────
+
+import express from 'express';
+
+// ─── Create Express App ─────────────────────────────────────────────────────────
+
+const app = express();
+export default app;
