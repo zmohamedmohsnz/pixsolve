24b9549984465c98b53a60d19673390cf3fe585c
zmuhsenz <zmohamedmohsnz@gmail.com>
2026-09-01 16:29:54 +0300

    feat: add helmet and express.json middleware
    - `helmet` : mitigate browser-based attacks by setting some HTTP response headers.
    - `express.json` : parses JSON request bodies.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 // ─── Import Modules ─────────────────────────────────────────────────────────────
 
 import express from 'express';
+import helmet from 'helmet';
 
 // ─── Create Express App ─────────────────────────────────────────────────────────
 
 const app = express();
+
+// ─── GLOBAL MIDDLEWARE ──────────────────────────────────────────────────────────
+
+// returns some response headers that mitigate browser-based attacks.
+app.use(helmet());
+
+// parses the JSON bodies.
+app.use(express.json({ limit: '10kb' }));
+
 export default app;
