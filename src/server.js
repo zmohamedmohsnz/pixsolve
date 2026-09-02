377ba8263fa8951ae3ffedbe1a62e9259957b247
zmuhsenz <zmohamedmohsnz@gmail.com>
2026-09-02 21:02:12 +0300

    refactor(log): replace console logging with pino logging

--- src/server.js
+++ src/server.js
@@ -3,21 +3,27 @@
 import app from './app.js';
 import config from './config/env.js';
 import connectToDB from './config/database.js';
+import logger from './config/logger.js';
 
 // ─── Connect Database ───────────────────────────────────────────────────────────
 
 try {
   await connectToDB(config.dbUri);
 } catch (err) {
-  // logger.fatal({ err }, 'Database connection failed');
-  console.error('Database connection failed');
+  logger.fatal(
+    { error: { name: err.name, code: err.code } },
+    'Database connection failed'
+  );
 
   // removing this line makes server starts regardless connection status
-  process.exit(1); 
+  process.exitCode = 1; 
 }
 
 // ─── Start a Server ─────────────────────────────────────────────────────────────
 
 const server = app.listen(config.port, () => {
-  console.log(`App running on port ${config.port}...`);
+  logger.info(
+    { server: { port: config.port, env: config.nodeEnv } },
+    'Server started'
+  );
 });
