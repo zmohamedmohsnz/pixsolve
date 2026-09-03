55de05c8deb4a216d29725cfaf8f6d027205f3fa
zmuhsenz <zmohamedmohsnz@gmail.com>
2026-09-03 04:09:01 +0300

    feat(error): handle uncaught exceptions and unhandled promise rejections

--- src/server.js
+++ src/server.js
@@ -5,6 +5,64 @@ import config from './config/env.js';
 import connectToDB from './config/database.js';
 import logger from './config/logger.js';
 
+let server;
+let isShuttingDown = false;
+
+// ─── Handle uncaught exceptions ─────────────────────────────────────────────────
+
+// these are the errors that happened in sync code
+// wether before express start or inside a callback.
+//
+// we terminate because they may leave app in inconsistent state.
+process.on('uncaughtException', (err) => {
+  logger.fatal(
+    { event: 'uncaughtException', err },
+    'Uncaught Exception Occurred'
+  );
+
+  process.exit(1);
+});
+
+// ─── Handle Unhandled Promise Rejection ─────────────────────────────────────────
+
+// these errors that happen when rejected promises have no rejection handler.
+process.on('unhandledRejection', (reason) => {
+  // this line prevents the shutdown procedure from starting more than once
+  // as if two promise rejects happen at the same time, so the handler could
+  // log multiple times, call `server.close()` more than once.
+  // it effectively means: “If shutdown has already started, do nothing.”
+  if (isShuttingDown) return;
+
+  isShuttingDown = true;
+
+  // we normalize the non-error to an error to pass an error object
+  // to pino logger as it handled error objects much better than strings
+  const err = 
+    reason instanceof Error
+    ? reason
+    : new Error(`Promise rejected with a non-Error value: ${String(reason)}`);
+
+  logger.fatal(
+    { event: 'unhandledRejection', err },
+    'Unhandled promise rejection occurred'
+  );
+
+  // stop process immediately if server is not listening
+  if (!server?.listening) {
+    return process.exit(1);
+  }
+
+  // if server is listening, stop accepting new connections
+  // and wait for existing connections to finish before terminating.
+  server.close(() => { process.exit(1); });
+
+  // if the exist connections lasting longer 
+  // than 10 seconds, terminate the process.
+  setTimeout(() => {
+    process.exit(1);
+  }, 10000).unref();
+});
+
 // ─── Connect Database & Start a Server ──────────────────────────────────────────
 
 const startServer = async () => {
@@ -29,4 +87,4 @@ const startServer = async () => {
   }
 };
 
-const server = await startServer();
+server = await startServer();
